a39ba4151ebb47cbad5e4053c69a09dc0604d79d
Paul Demchuk <Toadums@hotmail.com>
2013-03-09 14:26:48 -0800

    r4 bubble class set up
    nuff said

---
+++ BubbleBasket-WP8/BubbleBasket-WP8/BubbleBasket-WP8/Source.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleBasket_WP8
+{
+    class Source
+    {
+    }
+}
